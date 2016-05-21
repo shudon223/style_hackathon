a356b5cec9e20ba9de3402ffc4b4c10ec69bcbb6
shudon223 <shudonbrown@students.ptechnyc.org>
2016-05-21 16:41:18 +0000

    initial commit

---
+++ Javascript.js
@@ -0,0 +1,23 @@
+// JavaScript Fi/
+
+
+
+
+var character=[];
+
+var shoes = ["http://images.clipartpanda.com/shoe-clipart-The-gallery-for-ladies-shoes-clipart.jpg"];
+var shirts=[""];
+var pants=[""];
+var hats = ["http://cliparts.co/cliparts/8cA/ErE/8cAErEaLi.jpg"];
+function nextShoes(){
+ var shoesi=0;
+ 
+     
+ }
+ 
+
+
+var currentShoes;
+var currentShirt;
+var currentPants;
+var currentCharacter;
